cc0dcc30f977c567b01fcb422a07ff12637220b7
Geir Arne Brevik <geirarne@mesen.no>
2012-09-25 16:06:33 +0200

    support for email_field and url_field

--- lib/mesen_forms/form_builder.rb
+++ lib/mesen_forms/form_builder.rb
@@ -2,7 +2,7 @@ module MesenForms
   class FormBuilder < ::ActionView::Helpers::FormBuilder
     delegate :content_tag, :button_tag, :submit_tag, :link_to, :current_user, :to => :@template
     
-    %w[text_area text_field password_field collection_select].each do |method_name|
+    %w[text_area text_field email_field url_field password_field collection_select].each do |method_name|
       define_method(method_name) do |attribute, *options|
         opts = options.extract_options!
         if opts[:skip_label]
