12bcce62bd5f13a2534ce3da97a70588c14a20d6
kpilka <kathrin.pilka@gmail.com>
2013-09-18 10:08:52 +0200

    ckeditor: &nbsp;

--- lib/mesen_helper/helpers/form_helper.rb
+++ lib/mesen_helper/helpers/form_helper.rb
@@ -97,6 +97,10 @@ module MesenHelper
           str = simple_format str
         end
 
+        if /\s&nbsp;\s/.match(str)
+          str.gsub!(/\s&nbsp;\s/, '')
+        end
+
         # remove empty paragraphs
         if /<p>\s*(&nbsp;)?\s*<\/p>/.match(str)
           str.gsub!(/<p>\s*(&nbsp;)?\s*<\/p>/, '<br>')
@@ -146,6 +150,7 @@ module MesenHelper
         if /<p>\s*(&nbsp;)?\s*<\/p>/.match(str)
           str.gsub!(/<p>\s*(&nbsp;)?\s*<\/p>/, '')
         end
+
         # wrap in paragraphs if none is present
         if str !~/<p>/
           str = simple_format str
